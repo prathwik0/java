6c0b7e9ad88ee0fa472ff547da43d77edb5d48c8
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-06-14 23:29:33 +0530

    Create MethodOverriding.java

---
+++ Week2/MethodOverriding.java
@@ -0,0 +1,42 @@
+/* Write a java program to implement method overriding. */
+
+class ModOve {
+    int i, j;
+
+    ModOve(int a, int b) {
+        i = a;
+        j = b;
+    }
+
+    // display i and j
+    void show() {
+        System.out.println("i and j: " + i + " " + j);
+    }
+}
+
+
+class Another extends ModOve {
+    int k;
+
+    Another(int a, int b, int c) {
+        super(a, b);
+        k = c;
+    }
+
+    // display k – this overrides show() in A
+    void show() {
+        super.show();
+        System.out.println("k: " + k);
+    }
+}
+
+
+public class MethodOverriding {
+    public static void main(String args[]) {
+
+        Another obj = new Another(1, 2, 3);
+
+        // this calls show() method in Another
+        obj.show();
+    }
+}
